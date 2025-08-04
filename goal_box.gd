820f2bec0cf0fa852e9385bddcca5e002bc24049
skooter500 <skooter500@gmail.com>
2025-08-04 22:11:32 +0100

    faces

--- goal_box.gd
+++ goal_box.gd
@@ -1,8 +1,22 @@
 extends Area3D
 
+@export var image:Texture 
+
+func set_texture(mesh:MeshInstance3D):
+	var mat:StandardMaterial3D = mesh.get_surface_override_material(0)
+	mat = mat.duplicate()
+	mat.albedo_texture = image
+	mesh.set_surface_override_material(0, mat)
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	set_texture($front)
+	set_texture($back)
+	set_texture($left)
+	set_texture($right)
+	set_texture($top)
+	set_texture($bot)
 	pass # Replace with function body.
 
 
