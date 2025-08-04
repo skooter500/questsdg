93f75c41c9dfdb9fcf8d143c6579beea46f43fe7
skooter500 <skooter500@gmail.com>
2025-08-04 21:50:46 +0100

    goal box

--- start_xr.gd
+++ start_xr.gd
@@ -13,6 +13,10 @@ func enable_passthrough() -> bool:
 			return true
 		else:
 			return false
+	get_viewport().transparent_bg = true
+	environment.background_mode = Environment.BG_CLEAR_COLOR
+	environment.ambient_light_source = Environment.AMBIENT_SOURCE_COLOR
+	
 
 
 # Called when the node enters the scene tree for the first time.
