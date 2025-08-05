53d33a7d55787cb7a17643be5388b1b9f8701d48
skooter500 <skooter500@gmail.com>
2025-08-05 23:13:10 +0100

    sprite frames

--- goal_box.gd
+++ goal_box.gd
@@ -2,12 +2,19 @@ extends Area3D
 
 @export var image:Texture 
 
+var inside:bool = false
+
+var mats = []
+
 func set_texture(mesh:MeshInstance3D):
 	var mat:StandardMaterial3D = mesh.get_surface_override_material(0)
 	mat = mat.duplicate()
 	mat.albedo_texture = image
 	mesh.set_surface_override_material(0, mat)
+	mats.push_back(mat)
 	
+var left:Hand
+var right:Hand
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,24 +24,49 @@ func _ready() -> void:
 	set_texture($scaler/right)
 	set_texture($scaler/top)
 	set_texture($scaler/bot)
+	
+	left = $"../../XROrigin3D/left"
+	right = $"../../XROrigin3D/right"
 	pass # Replace with function body.
 
+var fade_out_tween:Tween = null
+
+func make_invisible():
+	monitoring = false        # Stops detecting other bodies entering/exiting
+	monitorable = false 
+	
+func fade_out():
+	if fade_out_tween:
+		return
+	else:
+		fade_out_tween = create_tween().set_trans(Tween.TRANS_LINEAR)
+		$selected_sound.pitch_scale = 0.5
+		$selected_sound.play()
+		for mat in mats:
+			fade_out_tween.tween_property(mat, "albedo_color:a", 0.0, 2.0)
+			fade_out_tween.set_parallel(true)
+			# Optional: Hide the mesh when fade completes
+		fade_out_tween.finished.connect(make_invisible)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# rotate_y(delta)
 	# rotate_x(delta)
+	
+	if inside && (left.selected || right.selected):
+		fade_out()		
 	pass
 
 
-func _on_area_entered(area: Area3D) -> void:
-	play_sound()
+func _on_area_entered(area: Area3D) -> void:	
 	if area.name.contains("hand"):
+		play_sound()
 		var t = create_tween() \
 			.set_ease(Tween.EASE_IN_OUT) \
 			.set_trans(Tween.TRANS_QUINT)
 		scale = Vector3.ONE
 		t.tween_property(self, "scale", big_scale, 1)
+		inside = true
 	pass # Replace with function body.
 
 var big_scale = Vector3(1.25, 1.25, 1.25)
@@ -44,11 +76,13 @@ func play_sound():
 	$AudioStreamPlayer3D.play()
 
 func _on_area_exited(area: Area3D) -> void:
-	play_sound()
+	
 	if area.name.contains("hand"):
+		play_sound()
 		var t = create_tween() \
 			.set_ease(Tween.EASE_IN_OUT) \
 			.set_trans(Tween.TRANS_QUINT)
 		scale = big_scale
 		t.tween_property(self, "scale", Vector3.ONE, 1)
+		inside = false
 	pass # Replace with function body.
---
+++ hand.gd
@@ -0,0 +1,28 @@
+class_name Hand
+
+extends XRNode3D
+
+@export var selected = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_hand_pose_detector_pose_started(p_name: String) -> void:
+	print(p_name + " started")
+	if p_name == "ThumbsUp":
+		selected = true
+	pass # Replace with function body.
+
+
+func _on_hand_pose_detector_pose_ended(p_name: String) -> void:
+	print(p_name + " ended")
+	if p_name == "ThumbsUp":
+		selected = false
+	pass # Replace with function body.
