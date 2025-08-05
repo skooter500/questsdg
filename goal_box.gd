8625bf06855ad087fdc420e66a0925f8598f01ea
skooter500 <skooter500@gmail.com>
2025-08-05 16:59:25 +0100

    hands and swooshing

--- goal_box.gd
+++ goal_box.gd
@@ -25,3 +25,30 @@ func _process(delta: float) -> void:
 	# rotate_y(delta)
 	# rotate_x(delta)
 	pass
+
+
+func _on_area_entered(area: Area3D) -> void:
+	play_sound()
+	if area.name.contains("hand"):
+		var t = create_tween() \
+			.set_ease(Tween.EASE_IN_OUT) \
+			.set_trans(Tween.TRANS_QUINT)
+		scale = Vector3.ONE
+		t.tween_property(self, "scale", big_scale, 1)
+	pass # Replace with function body.
+
+var big_scale = Vector3(1.25, 1.25, 1.25)
+
+func play_sound():
+	$AudioStreamPlayer3D.pitch_scale = randf_range(0.8, 1.2)
+	$AudioStreamPlayer3D.play()
+
+func _on_area_exited(area: Area3D) -> void:
+	play_sound()
+	if area.name.contains("hand"):
+		var t = create_tween() \
+			.set_ease(Tween.EASE_IN_OUT) \
+			.set_trans(Tween.TRANS_QUINT)
+		scale = big_scale
+		t.tween_property(self, "scale", Vector3.ONE, 1)
+	pass # Replace with function body.
