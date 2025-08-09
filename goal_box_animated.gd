09931929e6470079aeb81e950c7549d7dffe2373
skooter500 <skooter500@gmail.com>
2025-08-09 22:36:43 +0100

    fix node paths

--- goal_box_animated.gd
+++ goal_box_animated.gd
@@ -16,9 +16,24 @@ func bounce_in():
 			# Optional: Hide the mesh when fade completes
 		# fade_tween.finished.connect(make_invisible)
 
+func setup_vid_screen(vid_screen, view_port):
+	var m:StandardMaterial3D = vid_screen.get_surface_override_material(0)
+	var t:ViewportTexture = m.albedo_texture
+	var p = "animated_goals/" + name + "/" + view_port
+	print(t.viewport_path)
+	print(p)
+	t.viewport_path = p
+	vid_screen.set_surface_override_material(0, m)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
-	
+	setup_vid_screen($scaler/vid_screen, "SubViewport")
+	setup_vid_screen($scaler/vid_screen2, "SubViewport2")
+	setup_vid_screen($scaler/vid_screen3, "SubViewport")
+	setup_vid_screen($scaler/vid_screen4, "SubViewport2")
+	setup_vid_screen($scaler/vid_screen5, "SubViewport")
+	setup_vid_screen($scaler/vid_screen6, "SubViewport2")
 	bounce_in()
 	
 	
@@ -28,5 +43,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate_y(delta)
-	# rotate_x(delta)
+	rotate_x(delta)
 	pass
