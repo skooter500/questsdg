38af2a8be8d9b042bb894be37c815db7e7c0305e
skooter500 <bryan.duggan@tudublin.ie>
2025-08-12 23:38:48 +0100

    splash

--- xr_camera_3d.gd
+++ xr_camera_3d.gd
@@ -2,22 +2,18 @@ extends XRCamera3D
 
 var distance_in_front = -1
 
+@export var spawn_in_front:Node3D 
+
 func _ready() -> void:
 	await get_tree().process_frame
 	var y = global_position.y
 	print("Head pos" + str(global_position))
 	var in_front = global_position + (global_basis.z * distance_in_front)
 	in_front.y = y
-	var goals  = $"../../goals"  
 	
 	
-	goals.global_position = in_front
+	spawn_in_front.global_position = in_front
 	
 	var y_rotation = global_basis.get_euler().y
-	goals.global_basis = Basis(Vector3.UP, y_rotation + PI)
-	
-	
-	
-	
-	
+	spawn_in_front.global_basis = Basis(Vector3.UP, y_rotation + PI)
 	
